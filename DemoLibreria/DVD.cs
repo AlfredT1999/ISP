c2b34373931d1fbf9b7ca50e0e7318bb9bef5b5f
Jose Alfredo Tapia Hernandez <e-jatapia@neoris.com>
2022-01-18 12:47:09 -0600

    First setup

---
+++ DemoLibreria/DVD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoLibreria
+{
+    public class DVD : IArticulosLibreria
+    {
+        public string LibreriaId { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public DateTime FechaDePrestacion { get; set; }
+        public string NombreUsuarioAparta { get; set; }
+        public int FechaDeDevolucion { get; set; } = 14;
+        public int Paginas { get; set; }
+        public List<string> Actores { get; set; }
+        public int DuracionEnMinutos { get; set; }
+
+        public void Registro()
+        {
+            NombreUsuarioAparta = "";
+        }
+
+        public void Verificar(string nombreUsuarioAparta)
+        {
+            NombreUsuarioAparta = nombreUsuarioAparta;
+            FechaDePrestacion = DateTime.Now;
+        }
+
+        public DateTime FechaDeVencimiento()
+        {
+            return FechaDePrestacion.AddDays(FechaDeDevolucion);
+        }
+    }
+}
---
+++ DemoLibreria/LibroReferencia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DemoLibreria
+{
+    public class LibroReferencia : IArticulosLibreria
+    {
+        public string LibreriaId { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public DateTime FechaDePrestacion { get; set; }
+        public string NombreUsuarioAparta { get; set; }
+        public int FechaDeDevolucion { get; set; } = 14;
+        public int Paginas { get; set; }
+
+        public void Registro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verificar(string nombreUsuarioAparta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime FechaDeVencimiento()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
